96a5d65ddf76fedbe0eba35d0003ae8aa2be08d2
farahelghobashy <85609671+farahelghobashy@users.noreply.github.com>
2021-06-09 09:44:37 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var getusername = prompt("Hello! Who are You? Please state your name:")
+
+console.log(getusername)
+
+document.getElementById("username").innerText=getusername;
+
+// tooltip// 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  
+
